3df0c7aafdd0c5c9b7aac4c1c1b11bef66c0072e
Marilia Duarte Rodrigues <mariliaduarterod@gmail.com>
2023-04-23 14:01:15 -0300

    Operadores Condicional

---
+++ modulo-4/4.3-aula/condicional.js
@@ -0,0 +1,29 @@
+/**
+ * arquivo: tipoDados.js
+ * descrição: explicação de como podemos     diferentes tipos de dados em JavaScript
+ * author: Marilia Duarte
+ * data: 25/04/2023
+ */
+
+// Condicional if
+
+/*let podeDirigir = 16;
+
+if (podeDirigir >= 18) {
+    console.log('Tem permissão para dirigir')
+}*/
+
+// Condicioal if-else
+/*let podeDirigir = 16;
+
+if (podeDirigir >= 18) {
+    console.log('Tem permissão para dirigir')
+} else {
+    console.log('Você não tem permissão para dirigir!');
+}*/
+
+// Condicional operador (ternario)
+let idade = 16;
+let podeDirigir = idade >= 18
+? console.log ('Tem permissão para dirigir!') 
+: console.log ('Você não tem permissão para dirigir');
