9c8262f93a99f1b0341bdaab3484fe0ff7b0111a
Marilia Duarte Rodrigues <mariliaduarterod@gmail.com>
2023-04-23 15:09:48 -0300

    Laco While

---
+++ modulo-4/4.7-aula/lacoWhile.js
@@ -0,0 +1,24 @@
+/**
+ * arquivo: tipoDados.js
+ * descrição: explicação de como podemos     diferentes tipos de dados em JavaScript
+ * author: Marilia Duarte
+ * data: 23/04/2023 Domingo
+ */
+
+// while -> a condição será testada no início de cada interação do loop
+
+/*let numeroInicial = 1;
+
+while (numeroInicial <= 10) {
+    console.log('O número é... :' ,  numeroInicial);
+    numeroInicial++;
+}*/
+
+// do while -> sempre será executado uma vez
+
+let numeroInicial = 1;
+
+do {
+    console.log('O número é... :' ,  numeroInicial);
+    numeroInicial++;
+} while (numeroInicial <= 10);
