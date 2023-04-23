6ae94cbf3cf13f5dbdc5441566e44688ce0c0ea7
Marilia Duarte Rodrigues <mariliaduarterod@gmail.com>
2023-04-22 22:13:32 -0300

    tipoDados

---
+++ modulo-2/2.5-aula/tipoDados.js
@@ -0,0 +1,65 @@
+/**
+ * arquivo: tipoDados.js
+ * descrição: explicação de como podemos diferentes tipos de dados em JavaScript
+ * author: Marilia Duarte
+ * data: 25/04/2023
+ */
+
+//String
+
+/*'Olá'
+"Olá"
+`Olá`*/
+
+
+/*let nome = 'Marilia Duarte';
+let nomeMae = "Marli Duarte";
+let familia = `Eu me chamo ${nome} e o nome da minha mãe é ${nomeMae}`;
+
+console.log(familia);*/
+
+//Number
+
+/*const numero1 = 4;
+const numero2 = 3.99;
+const numero3 = 3e5; 
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);*/
+
+
+//BigInt
+/*const valor1 = 9071245475215457154545424514541n;
+console.log(valor1);//typeof*/
+
+//Boolean
+
+/*let x = 5;
+let y = 5;
+let z = 6;
+
+console.log((x == y));
+console.log((x == z));*/
+
+//Undefinied
+
+/*let nome;
+console.log(nome);
+// Null
+const numero = null;
+console.log(numero);*/
+
+
+//Object
+
+const estudante = {
+    nome: 'Mari',
+    sobrenome: 'Duarte',
+    idade: 34, 
+    curso: 'JavaScript',
+    valorcurso: 25.00,
+    ativo: true 
+};
+
+console.log(estudante);
