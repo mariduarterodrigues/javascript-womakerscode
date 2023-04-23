404fb37ad61178f099d77f04b70ac9a1a0aa9509
Marilia Duarte Rodrigues <mariliaduarterod@gmail.com>
2023-04-23 13:41:44 -0300

    Operadores Logicos

---
+++ modulo-3/3.7-aula/operadoresLogicos.js
@@ -0,0 +1,23 @@
+/**
+ * arquivo: tipoDados.js
+ * descrição: explicação de como podemos     diferentes tipos de dados em JavaScript
+ * author: Marilia Duarte
+ * data: 25/04/2023
+ */
+
+
+// Operador And &&
+console.log(true && true);
+console.log(false && true);
+console.log(true && false);
+console.log(false && false);
+
+// Operador Or || 
+/*console.log(true || true);
+console.log(false || true);
+console.log(true || false);
+console.log(false || false); */
+
+// Operador Not !
+console.log(!true);
+console.log(!false);
