8f88b1e2fd2d1b42ea34864463d43d5c7bc47faf
Marilia Duarte Rodrigues <mariliaduarterod@gmail.com>
2023-04-23 13:00:57 -0300

    Operadores Aritmeticos

---
+++ modulo-3/3.3-aula/operadoresaritmeticos.js
@@ -0,0 +1,40 @@
+/**
+ * arquivo: tipoDados.js
+ * descrição: explicação de como podemos     diferentes tipos de dados em JavaScript
+ * author: Marilia Duarte
+ * data: 25/04/2023
+ */
+
+
+let num1 = 20;
+let num2 = 10;
+
+//Adição + 
+
+let totaladicao = num1 + num2;
+console.log(totaladicao);
+ 
+// Subtração -
+
+let totalsubtracao = num1 - num2;
+console.log(totalsubtracao);
+
+//Multiplicação *
+
+let totalmultiplicação = num1 * num2;
+console.log(totalmultiplicação);
+
+//Divisão /
+
+let totaldivisao = num1 / num2;
+console.log(totaldivisao);
+
+//Incremento ++ Loops for, do-do while
+
+console.log(num1++);
+console.log(++num1);
+
+//Decremento --
+
+console.log(num2--);
+console.log(--num2);
