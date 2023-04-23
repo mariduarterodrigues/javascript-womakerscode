65f61f71f3355b962aa8c0b699774a8aebf0443d
Marilia Duarte Rodrigues <mariliaduarterod@gmail.com>
2023-04-23 13:16:03 -0300

    Operadores Comparacao

---
+++ modulo-3/3.5-aula/operadorescomparacao.js
@@ -0,0 +1,28 @@
+/**
+ * arquivo: tipoDados.js
+ * descrição: explicação de como podemos     diferentes tipos de dados em JavaScript
+ * author: Marilia Duarte
+ * data: 25/04/2023
+ */
+
+
+// Igual a ==
+/*console.log (10 == 10);
+console.log (12 == 10);
+
+//Diferente de !=
+console.log (10 != 10);
+console.log (10 != 9);
+
+// Maior que  > 
+console.log (10 > 9);
+console.log (9 > 10);
+
+// Menor que < 
+console.log (10 < 9);
+console.log (9 < 10);*/
+
+
+// Valor e tipo igual a ===
+console.log(10 === 10);
+console.log (10 === 'Marilia');
