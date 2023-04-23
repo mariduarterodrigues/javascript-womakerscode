4a113ea56d00380de21cac3d865d17669ed2a1b4
Marilia Duarte Rodrigues <mariliaduarterod@gmail.com>
2023-04-22 21:00:12 -0300

    Modulo-2-Variaveis JavaScript

---
+++ modulo-2/2.3-aula/variaveis.js
@@ -0,0 +1,31 @@
+//Uso de variáveis em JS
+
+//SOMA = num1 + num2
+
+/*var num1 = 10;
+var num2 = 20;
+
+var total = num1 + num2;
+console.log(total);*/
+
+//let 
+/*let saudacao = "Olá, progamadoras";
+
+if (true) {
+    let saudacao = "Quero aprender JavaScript";
+    console.log(saudacao);
+}
+
+console.log(saudacao);*/
+
+//const
+
+const cidade = "Hell de Janeiro";
+
+function estado () {
+    const cidade = "São Paulo";
+    console.log(cidade);    
+}
+
+estado();
+console.log(cidade);
