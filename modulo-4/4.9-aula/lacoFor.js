941088e6d2191f81274b96ed76ee564748967af0
Marilia Duarte Rodrigues <mariliaduarterod@gmail.com>
2023-04-23 15:17:44 -0300

    Laco For

---
+++ modulo-4/4.9-aula/lacoFor.js
@@ -0,0 +1,12 @@
+/**
+ * arquivo: tipoDados.js
+ * descrição: explicação de como podemos     diferentes tipos de dados em JavaScript
+ * author: Marilia Duarte
+ * data: 23/04/2023 Domingo
+ */
+
+// Laço For
+
+for(let i=1; i <= 10; i++){
+    console.log('O número é...' , i);
+}
